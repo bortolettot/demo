6b93f3e703be0d2ed43d8a0cf743130328b343f8
Thiago Bortoletto <thiago.bortoletto@dextra-sw.com>
2021-02-15 13:57:23 -0300

    teste

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.MainService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    private final MainService service;
 
-    @RequestMapping("/")
-    public String index(){
-        return "Estou ak";
+    public MainController(MainService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(value = "/", params = "as")
+    public String index(String as){
+        return  service.index(as);
     }
 
 }
