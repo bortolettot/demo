a0133caf0778eba967d6a2ea69da213cd86ce46b
Thiago Bortoletto <thiago.bortoletto@dextra-sw.com>
2021-02-15 13:32:26 -0300

    vai

---
+++ src/main/java/com/example/demo/TesteSonarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSonarApplication.class, args);
+	}
+
+}
